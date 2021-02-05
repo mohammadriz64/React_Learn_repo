c8e47ff30e309e13b5b7c8945c2d6e69522eb31c
mohammadriz64 <76685820+mohammadriz64@users.noreply.github.com>
2021-02-05 15:56:57 +0530

    onClick event

--- src/Components/Child/ChildVarr.jsx
+++ src/Components/Child/ChildVarr.jsx
@@ -6,6 +6,7 @@ export const ChildVarr = (item) => {
             <h2>Props with functional Component</h2>
             <p>Hello!!! I am a {item.name} of {item.field} </p>
             <p>I'm learning {item.technology} </p>
+            
         </div>
     );
 };
--- src/Components/PropsWithClassComp/ClassCompParent.jsx
+++ src/Components/PropsWithClassComp/ClassCompParent.jsx
@@ -1,26 +1,48 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import ClassCompChild from "../PropsWithClassComp/ClassCompChild";
 
 export default class ClassCompParent extends Component {
-    state= {
-        person:[
-            {name:"Thor", age: 35},
-            {name:"Iron-Man", age: 36},
-            {name:"Spider-Man", age: 25},
-            {name:"SuperMan", age: 40},
-        ],
-    }
+  state = {
+    person: [
+      { name: "Thor", age: 35 },
+      { name: "Iron-Man", age: 36 },
+      { name: "Spider-Man", age: 25 },
+      { name: "SuperMan", age: 40 },
+    ],
+  };
+  changeNameHandler =() => {
+      this.setState({
+          person :[
+            { name: "HeMan", age: 40 },
+            { name: "Loki", age: 45 },
+            { name: "Lee", age: 30 },
+            { name: "Bruce", age: 40 },
+          ],
 
-    render() {
-        return (
-            <div>
-                
-<ClassCompChild name={this.state.person[0].name} age={this.state.person[0].age} />      
-<ClassCompChild name={this.state.person[1].name} age={this.state.person[1].age} />  
-<ClassCompChild name={this.state.person[2].name} age={this.state.person[1].age} />
-<ClassCompChild name={this.state.person[3].name} age={this.state.person[3].age} />
+      });
+  };
 
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <ClassCompChild
+          name={this.state.person[0].name}
+          age={this.state.person[0].age}
+        />
+        <ClassCompChild
+          name={this.state.person[1].name}
+          age={this.state.person[1].age}
+        />
+        <ClassCompChild
+          name={this.state.person[2].name}
+          age={this.state.person[1].age}
+        />
+        <ClassCompChild
+          name={this.state.person[3].name}
+          age={this.state.person[3].age}
+        />
+        <button onClick={this.changeNameHandler} >ClickButton</button>
+      </div>
+    );
+  }
 }
