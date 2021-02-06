ea1c6cedfdf2d7231fa860b7047c85421edd00aa
mohammadriz64 <76685820+mohammadriz64@users.noreply.github.com>
2021-02-06 18:24:19 +0530

    working with method reference

---
+++ src/Components/Aamer.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Aamer extends Component {
+    render() {
+        return (
+            <div>
+                <label>I'm Rizwan here</label>
+                <input onChange={this.props.} type="text"/>
+                
+            </div>
+        )
+    }
+}
