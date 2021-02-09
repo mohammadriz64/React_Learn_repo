009779a2174e3901e985e0311d2ec0900e8e7d0e
mohammadriz64 <76685820+mohammadriz64@users.noreply.github.com>
2021-02-09 15:37:52 +0530

    useState testing using console

--- src/Components/ConditinalRendering/CondRenParFun.jsx
+++ src/Components/ConditinalRendering/CondRenParFun.jsx
@@ -2,8 +2,16 @@ import React,{ useState } from 'react';
 import { CondRenChilFun } from './CondRenChilFun';
 
 export const CondRenParFun = () => {
+    const [personData,setPersonData]=useState("Mohammad Rizwan");
+    const handleNameChange=()=>{
+        setPersonData(personData);
+
+    };
     return (
         <div>
+            <button onClick={handleNameChange}></button>
+           <CondRenChilFun/> 
+           <CondRenChilFun/> 
            <CondRenChilFun/> 
         </div>
     );
