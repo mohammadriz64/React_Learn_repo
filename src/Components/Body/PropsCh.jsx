4c14e160e450274fbfad5e2d192f7d97ef3e39da
mohammadriz64 <76685820+mohammadriz64@users.noreply.github.com>
2021-02-09 13:27:51 +0530

    passing other information within child import

--- src/Components/Body/PropsCh.jsx
+++ src/Components/Body/PropsCh.jsx
@@ -4,7 +4,7 @@ const PropsCh = (props) => {
     return (
         <div>
           <h4>Hello {props.name} a.k.a {props.heroName}</h4>
-          {props.Children}  
+          {props.children}  
         </div>
     );
 };
--- src/Components/Body/PropsPar.jsx
+++ src/Components/Body/PropsPar.jsx
@@ -4,10 +4,13 @@ import PropsCh from './PropsCh';
 const PropsPar = () => {
     return (
         <div>
-            <PropsCh name="Bruce" heroName="Batman"/>
-            <PropsCh name="John" heroname="Spiderman"/>
+            <PropsCh name="Bruce" heroName="Batman">
+            <p>This is childern Props..!!!!</p>
+            </PropsCh>
+            <PropsCh name="John" heroname="Spiderman">
+                <button>Action</button>
+                </PropsCh>
             <PropsCh name="Diana" heroName="Wonderwoman"/>
-            <p>This is Childern Props..!!!!</p>
         </div>
     );
 };
