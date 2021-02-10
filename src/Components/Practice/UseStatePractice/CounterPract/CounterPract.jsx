1c61e3bd6743f33d74041f597852ee258693cd56
mohammadriz64 <76685820+mohammadriz64@users.noreply.github.com>
2021-02-10 16:11:47 +0530

    adding and deleting number using useState

--- src/Components/Practice/UseStatePractice/CounterPract/CounterPract.jsx
+++ src/Components/Practice/UseStatePractice/CounterPract/CounterPract.jsx
@@ -2,15 +2,18 @@ import React,{useState} from 'react';
 
 const CounterPract = () => {
     const [addNumber,setAddNumber] = useState(0);
-    const handleCounter = () =>{
+    const handleAddCounter = () =>{
         setAddNumber(addNumber+1);
     }
+    const handleDeleteCounter = () =>{
+        setAddNumber(addNumber-1);
+    }
+
     return (
         <div>
-            <h1>Increase Number...</h1> <button>{addNumber}</button>
-            {/* <button onClick={handleCounter}>Add</button> */}
-            <h1>Decrease Number...</h1>
-            <button>Delete</button>
+            <h1>Increase Number...Decrease Number..</h1> <button>{addNumber}</button>
+            <button onClick={handleAddCounter}>Add</button>
+            <button onClick={handleDeleteCounter}>Delete</button>
         </div>
     );
 };
