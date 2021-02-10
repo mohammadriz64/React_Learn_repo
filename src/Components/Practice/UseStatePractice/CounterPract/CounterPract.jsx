638d4eef879231c9aef470162797974d95422235
mohammadriz64 <76685820+mohammadriz64@users.noreply.github.com>
2021-02-10 16:34:56 +0530

    loading image on button using useState

--- src/Components/Practice/UseStatePractice/CounterPract/CounterPract.jsx
+++ src/Components/Practice/UseStatePractice/CounterPract/CounterPract.jsx
@@ -11,13 +11,17 @@ const CounterPract = () => {
     const handleDelCounter = (delNumber) =>{
         setAddNumber(addNumber-delNumber);
     }
+    const handleImage = () =>{
+        setAddNumber(<img src="logo512.png"/>);
+    }
 
     return (
         <div>
             <h1>Increase Number...Decrease Number..</h1> <button>{addNumber}</button>
             <button onClick={handleAddCounter}>Add</button>
             <button onClick={handleDeleteCounter}>Delete</button>
-            <button onClick={handleDelCounter.bind(addNumber,2)}>Delete</button>
+            <button onClick={handleDelCounter.bind(addNumber,2)}>Delete</button> <br/> <br/>
+            <button onClick={handleImage}>Load Image</button>
         </div>
     );
 };
