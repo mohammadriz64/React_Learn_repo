5674a1b5af83dc6de0469c92d516616519ebaefa
mohammadriz64 <76685820+mohammadriz64@users.noreply.github.com>
2021-02-12 16:12:01 +0530

    adding values of array

--- src/Components/Practice/MapFunction/MapParent.jsx
+++ src/Components/Practice/MapFunction/MapParent.jsx
@@ -3,10 +3,10 @@ import { MapChild } from './MapChild';
 
 export const MapParent = () => {
     const personData =[
-        {Company="", Car=""}
-        {Company="", Car=""}
-        {Company="", Car=""}
-        {Company="", Car=""}
+        {Company="Audi", Car="A8"}
+        {Company="BMW", Car="M3"}
+        {Company="Hyundai", Car="i20"}
+        {Company="Honda", Car="Civic"}
 
     ];
     return (
