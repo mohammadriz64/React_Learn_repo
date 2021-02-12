e8c88dcefb7ec113bd5b2f2a8aede6791c9c37b6
mohammadriz64 <76685820+mohammadriz64@users.noreply.github.com>
2021-02-12 16:15:21 +0530

    details

--- src/Components/Practice/MapFunction/MapParent.jsx
+++ src/Components/Practice/MapFunction/MapParent.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 import { MapChild } from './MapChild';
 
 export const MapParent = () => {
-    const personData =[
-        {Company="Audi", Car="A8"}
-        {Company="BMW", Car="M3"}
-        {Company="Hyundai", Car="i20"}
-        {Company="Honda", Car="Civic"}
+    const [personData,setPersonData]=useState({
+        details=[
+            {Company="Audi", Car="A8"}
+            {Company="BMW", Car="M3"}
+            {Company="Hyundai", Car="i20"}
+            {Company="Honda", Car="Civic"}
+    
+        ];
 
-    ];
+    });
+   
     return (
         <div>
             <MapChild/>
