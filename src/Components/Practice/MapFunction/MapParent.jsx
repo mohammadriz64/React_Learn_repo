10fecba147ea002222268219a867f7a9f7d46b05
mohammadriz64 <76685820+mohammadriz64@users.noreply.github.com>
2021-02-12 16:23:57 +0530

    trouble shooting array

--- src/Components/Practice/MapFunction/MapParent.jsx
+++ src/Components/Practice/MapFunction/MapParent.jsx
@@ -3,13 +3,13 @@ import { MapChild } from './MapChild';
 
 export const MapParent = () => {
     const [personData,setPersonData]=useState({
-        details=[
-            {Company="Audi", Car="A8"}
-            {Company="BMW", Car="M3"}
-            {Company="Hyundai", Car="i20"}
-            {Company="Honda", Car="Civic"}
+        details:[
+            {Company:"Audi", Car:"A8"},
+            {Company:"BMW", Car:"M3"},
+            {Company:"Hyundai", Car:"i20"},
+            {Company:"Honda", Car:"Civic"},
     
-        ];
+        ],
 
     });
    
