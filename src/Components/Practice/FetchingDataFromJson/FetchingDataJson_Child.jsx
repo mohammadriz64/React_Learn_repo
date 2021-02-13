9af0d8f16fc1c69dad529d25431032f46f279c30
mohammadriz64 <76685820+mohammadriz64@users.noreply.github.com>
2021-02-13 16:10:36 +0530

    title

--- src/Components/Practice/FetchingDataFromJson/FetchingDataJson_Child.jsx
+++ src/Components/Practice/FetchingDataFromJson/FetchingDataJson_Child.jsx
@@ -7,7 +7,7 @@ export const FetchingDataJson_Child = () => {
             <Card style={{ width: '18rem' }}>
   <Card.Img variant="top" src="placement.png" />
   <Card.Body>
-    <Card.Title>Card Title</Card.Title>
+    <Card.Title>Placement</Card.Title>
     <Card.Text>
       Some quick example text to build on the card title and make up the bulk of
       the card's content.
