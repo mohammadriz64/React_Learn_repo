bc01f3f969e19003e6dd4710aab491084a3eaa95
mohammadriz64 <76685820+mohammadriz64@users.noreply.github.com>
2021-02-13 15:53:39 +0530

    linking files

--- src/Components/Practice/FetchingDataFromJson/FetchingDataJson_Child.jsx
+++ src/Components/Practice/FetchingDataFromJson/FetchingDataJson_Child.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 
-const FetchingDataJson_Child = () => {
+export const FetchingDataJson_Child = () => {
     return (
         <div>
             
         </div>
     );
 };
-
-export default FetchingDataJson_Child;
---
+++ src/Components/Practice/FetchingDataFromJson/FetchingDataJson_Parent.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { FetchingDataJson_Child } from './FetchingDataJson_Child';
+
+export const FetchingDataJson_Parent = () => {
+    return (
+        <div>
+            <FetchingDataJson_Child/>
+        </div>
+    );
+};
