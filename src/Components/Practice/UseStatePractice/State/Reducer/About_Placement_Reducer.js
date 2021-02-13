3e86cd28c2af0660ac982114f14ab104443f2887
mohammadriz64 <76685820+mohammadriz64@users.noreply.github.com>
2021-02-13 15:47:13 +0530

    complete data

--- src/Components/Practice/UseStatePractice/State/Reducer/About_Placement_Reducer.js
+++ src/Components/Practice/UseStatePractice/State/Reducer/About_Placement_Reducer.js
@@ -1,8 +1,30 @@
-const About_Placement_Reducer = {
-  visitingRecruiters: [
-    { id:"1asdf", name: "TCS", field: "IT", required: "Ten Candidates" },
-    { id:"2asdf", name: "Infosys", field: "IT", required: "Six Candidates" },
-    { id:"3asdf", name: "Tata", field: "Mechanical", required: "Eight Candidates" },
-    { id:"4asdf", name: "Kirlosker", field: "Mechanical", required: "Eleven Candidates" },
-  ],
+export const About_Placement_Reducer = {
+    visitingRecruiters: [{
+            id: "1asdf",
+            name: "TCS",
+            field: "IT",
+            required: "Ten Candidates"
+        },
+
+        {
+            id: "2asdf",
+            name: "Infosys",
+            field: "IT",
+            required: "Six Candidates"
+        },
+
+        {
+            id: "3asdf",
+            name: "Tata",
+            field: "Mechanical",
+            required: "Eight Candidates",
+        },
+        
+        {
+            id: "4asdf",
+            name: "Kirlosker",
+            field: "Mechanical",
+            required: "Eleven Candidates",
+        },
+    ],
 };
