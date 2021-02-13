28e2041a420afcfc035aac316a47931a570da612
mohammadriz64 <76685820+mohammadriz64@users.noreply.github.com>
2021-02-13 16:12:53 +0530

    paragraph

--- src/Components/Practice/FetchingDataFromJson/FetchingDataJson_Child.jsx
+++ src/Components/Practice/FetchingDataFromJson/FetchingDataJson_Child.jsx
@@ -9,8 +9,8 @@ export const FetchingDataJson_Child = () => {
   <Card.Body>
     <Card.Title>Placement</Card.Title>
     <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
+        This is to inform All students that many companies will visit our campus for hiring student in various fields.
+     
     </Card.Text>
     <Button variant="primary">Go somewhere</Button>
   </Card.Body>
