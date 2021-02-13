75cf01c44f3e4c90facde7131f5bdcf3e689be92
mohammadriz64 <76685820+mohammadriz64@users.noreply.github.com>
2021-02-13 15:44:08 +0530

    giving id for each array object

--- src/Components/Practice/UseStatePractice/State/Reducer/About_Placement_Reducer.js
+++ src/Components/Practice/UseStatePractice/State/Reducer/About_Placement_Reducer.js
@@ -1,8 +1,8 @@
 const About_Placement_Reducer = {
   visitingRecruiters: [
-    { name: "TCS", field: "IT", required: "Ten Candidates" },
-    { name: "Infosys", field: "IT", required: "Six Candidates" },
-    { name: "Tata", field: "Mechanical", required: "Eight Candidates" },
-    { name: "Kirlosker", field: "Mechanical", required: "Eleven Candidates" },
+    { id:"1asdf", name: "TCS", field: "IT", required: "Ten Candidates" },
+    { id:"2asdf", name: "Infosys", field: "IT", required: "Six Candidates" },
+    { id:"3asdf", name: "Tata", field: "Mechanical", required: "Eight Candidates" },
+    { id:"4asdf", name: "Kirlosker", field: "Mechanical", required: "Eleven Candidates" },
   ],
 };
