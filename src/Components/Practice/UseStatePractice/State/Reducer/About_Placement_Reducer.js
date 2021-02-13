77d0b9e9151aaa65dc3056e5c781fca2d2adcc4c
mohammadriz64 <76685820+mohammadriz64@users.noreply.github.com>
2021-02-13 15:40:50 +0530

    array syntax complete

--- src/Components/Practice/UseStatePractice/State/Reducer/About_Placement_Reducer.js
+++ src/Components/Practice/UseStatePractice/State/Reducer/About_Placement_Reducer.js
@@ -1,8 +1,8 @@
 const About_Placement_Reducer = {
-    visitingRecruiters:[
-        {name:"TCS", field:"IT", required:"Ten Candidates"},
-        {name:"Infosys", field:"IT", required:"Six Candidates"},
-        {name:"Tata", field:"Mechanical", required:"Eight Candidates"},
-        {name:"Kirlosker", field:"Mechanical", required:"Eleven Candidates"},
-    ],
-}
+  visitingRecruiters: [
+    { name: "TCS", field: "IT", required: "Ten Candidates" },
+    { name: "Infosys", field: "IT", required: "Six Candidates" },
+    { name: "Tata", field: "Mechanical", required: "Eight Candidates" },
+    { name: "Kirlosker", field: "Mechanical", required: "Eleven Candidates" },
+  ],
+};
