4573249030aac6c3cc3ef96efd0e6271c766259b
mohammadriz64 <76685820+mohammadriz64@users.noreply.github.com>
2021-02-13 15:31:58 +0530

    adding data

---
+++ src/Components/Practice/UseStatePractice/State/Reducer/About_Placement_Reducer.js
@@ -0,0 +1,8 @@
+const About_Placement_Reducer = {
+    visitingRecruiters:[
+        {name:"TCS", field:"IT"},
+        {name:"Infosys", field:"IT"},
+        {name:"Tata", field:"Mechanical"},
+        {name:"Kirlosker", field:"Mechanical"},
+    ],
+}
