1868d587691b995b9e599a28f35df8d22b2d879e
mohammadriz64 <76685820+mohammadriz64@users.noreply.github.com>
2021-02-15 19:42:02 +0530

    using useState

--- src/Components/Practice/UseStatePractice/CounterPract/IncrementDecrementNumber/IncDecNum.jsx
+++ src/Components/Practice/UseStatePractice/CounterPract/IncrementDecrementNumber/IncDecNum.jsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './IncDecNum.css';
 
 const IncDecNum = () => {
+    const [number,setNumber] = useState(0);
+
+   const addNumber=()=>{
+        setNumber(number+1)
+    }
 
     return (
         <div>
             <div className="main_div">
                 <div className="center_div">
                     <div className="btn_div">
-                        <h1></h1>
+                        <h1> {} </h1>
                         <button className="btn_style">Addition</button>
                         <button className="btn_style">Subtraction</button>
                     </div>
