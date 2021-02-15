02257f82ced7fff779b6dfcfa0d4fc0ae047904a
mohammadriz64 <76685820+mohammadriz64@users.noreply.github.com>
2021-02-15 19:07:48 +0530

    button styling

--- src/Components/Practice/UseStatePractice/CounterPract/IncrementDecrementNumber/IncDecNum.jsx
+++ src/Components/Practice/UseStatePractice/CounterPract/IncrementDecrementNumber/IncDecNum.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import './IncDecNum.css';
 
 const IncDecNum = () => {
-    
+
     return (
         <div>
             <div className="main_div">
                 <div className="center_div">
                     <div className="btn_div">
                         <h1></h1>
-                        <button>Addition</button>
-                        <button>Subtraction</button>
+                        <button className="btn_style">Addition</button>
+                        <button className="btn_style">Subtraction</button>
                     </div>
                 </div>
             </div>
