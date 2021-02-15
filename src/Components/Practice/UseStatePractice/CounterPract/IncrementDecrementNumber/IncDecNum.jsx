de7910ad02d3b953c204439e282e955f732c426b
mohammadriz64 <76685820+mohammadriz64@users.noreply.github.com>
2021-02-15 18:20:14 +0530

    making card with buttons

---
+++ src/Components/Practice/UseStatePractice/CounterPract/IncrementDecrementNumber/IncDecNum.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const IncDecNum = () => {
+    return (
+        <div>
+            <div className="main_div">
+                <div className="center_div">
+                    <div className="btn_div">
+                        <button>Addition</button>
+                        <button>Subtraction</button>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    );
+};
+
+export default IncDecNum;
