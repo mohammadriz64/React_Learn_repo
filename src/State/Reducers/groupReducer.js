941925cf728a5341c017aa9f4c80253e15b879ac
mohammadriz64 <76685820+mohammadriz64@users.noreply.github.com>
2021-02-16 15:53:38 +0530

    describing name in ovject

--- src/State/Reducers/groupReducer.js
+++ src/State/Reducers/groupReducer.js
@@ -5,7 +5,10 @@ const groupReducer =()=>{
 
 const groupsDefaultState={     //we can use groupsInitialState too
 groups:[
-    {},
+    {name:"Redux Practical for Begginers",
+     image:""
+
+},
     {},
     {},
 
