5b5e5ba7736f377224b02c66a3b72212e7948045
mohammadriz64 <76685820+mohammadriz64@users.noreply.github.com>
2021-02-16 16:08:59 +0530

    spread operator in switch case

--- src/State/Reducers/groupReducer.js
+++ src/State/Reducers/groupReducer.js
@@ -1,6 +1,7 @@
 const groupReducer =(state=groupsDefaultState,action)=>{
     switch(action,type){
         case "ADD_NEW_GROUP":
+            return{...state,}   //(...)this triple dot is spread operator to copy state
     }
 };
 
