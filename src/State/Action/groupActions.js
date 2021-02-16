9db5d7e4355bb30d0e46d78857f5144dd433180f
mohammadriz64 <76685820+mohammadriz64@users.noreply.github.com>
2021-02-16 15:42:03 +0530

    adding group values

--- src/State/Action/groupActions.js
+++ src/State/Action/groupActions.js
@@ -1,6 +1,10 @@
 const addNewGroup =()=>({
     // under double quotations must write capital letters
-    type:"ADD_NEW_GROUP"
+    type:"ADD_NEW_GROUP",
+    groups:{
+        name,
+        id,
+    }
 
 
 })
