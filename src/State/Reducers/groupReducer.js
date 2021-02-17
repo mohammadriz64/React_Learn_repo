1dffff6204edf962f2f0bf824f28e7fe5bf832e6
mohammadriz64 <76685820+mohammadriz64@users.noreply.github.com>
2021-02-17 15:57:54 +0530

    finding error

--- src/State/Reducers/groupReducer.js
+++ src/State/Reducers/groupReducer.js
@@ -1,24 +1,23 @@
 const groupReducer = (state = groupsDefaultState, action) => {
-    switch (action.type) {
-        case "ADD_NEW_GROUP":
-            //(...)this triple dot is spread operator to copy state
-            return {
-                ...state,
-                groups:[...state.groups,action.groups]
-            };
-            default:
-                return state;
-    }
+  switch (action.type) {
+    case "ADD_NEW_GROUP":
+      //(...)this triple dot is spread operator to copy state
+      return {
+        ...state,
+        groups: [...state.groups, action.groups],
+      };
+    default:
+      return state;
+  }
 };
 
-
-const groupsDefaultState = { //we can use groupsInitialState too
-    groups: [{
-            name: "Redux Practical for Begginers",
-            image: "placement.png",
-            desc: "recruiters image"
-
-        },
-
-    ],
-}
+const groupsDefaultState = {
+  //we can use groupsInitialState too
+  groups: [
+    {
+      name: "Redux Practical for Begginers",
+      image: "placement.png",
+      desc: "recruiters image",
+    },
+  ],
+};
