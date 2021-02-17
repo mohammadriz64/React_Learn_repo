78e474fffea889552b7b398c3c463716c3890c6c
mohammadriz64 <76685820+mohammadriz64@users.noreply.github.com>
2021-02-17 15:09:24 +0530

    configStore

--- src/State/Store/configStore.js
+++ src/State/Store/configStore.js
@@ -1,8 +1,11 @@
 import {createStore,combineReducer} from 'redux';
 const configStore =()=>{
-    const store = createStore({
-        combineReducer,
+    const store = createStore(
+        combineReducers({
         groupReducer,
     }),
-    
-}
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX__REDUX_DEVTOOLS__EXTENSION__()
+    );
+    return Store;
+
+};
