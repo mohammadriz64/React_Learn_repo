6aa8fb21ffe4fd012ca7d9c3bd86474a01e0a3f0
mohammadriz64 <76685820+mohammadriz64@users.noreply.github.com>
2021-02-20 15:34:30 +0530

    combine group reducer in store

--- src/State/Store/configStore.js
+++ src/State/Store/configStore.js
@@ -14,3 +14,14 @@
 
 // };
 
+import {createStore,combineReducers} from 'redux';
+import {groupReducer} from '../Reducers/groupReducer';
+export const configStore=()=>{
+    const store=createStore(
+        combineReducers({
+            groupReducer,
+        }),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    );
+    return store;
+}
