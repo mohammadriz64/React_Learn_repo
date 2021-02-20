52c0893ae25992480bff2ba1ecfc933915d38f94
mohammadriz64 <76685820+mohammadriz64@users.noreply.github.com>
2021-02-20 16:35:29 +0530

    successfully loaded data from redux

--- src/Components/Practice/FetchingFromRedux/FetchingDataReduxChild.jsx
+++ src/Components/Practice/FetchingFromRedux/FetchingDataReduxChild.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const FetchingDataReduxChild = ({itemCurrent}) => {
     return (
         <div>
-
-            
+            <h3>{itemCurrent.name}</h3>
+            <img style={{width:"400px",height:"350px"}} src={itemCurrent.image} alt="bike"/>
         </div>
     );
 };
