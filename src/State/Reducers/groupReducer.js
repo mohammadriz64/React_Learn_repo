7b01a3d5fa86ce799fe5a217015f483b90593e5d
mohammadriz64 <76685820+mohammadriz64@users.noreply.github.com>
2021-02-20 15:17:20 +0530

    working in reducer

--- src/State/Reducers/groupReducer.js
+++ src/State/Reducers/groupReducer.js
@@ -1,23 +1,46 @@
-const groupReducer = (state = groupsDefaultState, action) => {
-  switch (action.type) {
-    case "ADD_NEW_GROUP":
-      //(...)this triple dot is spread operator to copy state
-      return {
-        ...state,
-        groups: [...state.groups, action.groups],
-      };
-    default:
-      return state;
-  }
-};
+// const groupReducer = (state = groupsDefaultState, action) => {
+//   switch (action.type) {
+//     case "ADD_NEW_GROUP":
+//       //(...)this triple dot is spread operator to copy state
+//       return {
+//         ...state,
+//         groups: [...state.groups, action.groups],
+//       };
+//     default:
+//       return state;
+//   }
+// };
 
-const groupsDefaultState = {
-  //we can use groupsInitialState too
-  groups: [
-    {
-      name: "Redux Practical for Begginers",
-      image: "placement.png",
-      desc: "recruiters image",
-    },
+// const groupsDefaultState = {
+//   //we can use groupsInitialState too
+//   groups: [
+//     {
+//       name: "Redux Practical for Begginers",
+//       image: "placement.png",
+//       desc: "recruiters image",
+//     },
+//   ],
+// };
+
+const groupsDefaultState ={
+  groups:[
+    {name:"Aadil",
+     desc: "working with redux"
+  },
+    {name:"Faazil",
+     desc: "living in bangaluru"
+  },
+    {name:"Rizwam",
+     desc: "working with redux with Aadil"
+  },
   ],
 };
+
+export const groupReducer = (state=groupsDefaultState,action)=>{
+  switch (action.type){
+    case "ADD_NEW_GROUP":
+      return {...state,groups:[...state.groups,action.groups]};
+      default:
+        return state;
+  }
+}
