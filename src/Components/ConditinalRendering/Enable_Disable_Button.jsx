f7f9cdc21ad911f91fbbf627b439a68e5afdb88c
mohammadriz64 <76685820+mohammadriz64@users.noreply.github.com>
2021-02-22 21:30:10 +0530

    creating handle change event

--- src/Components/ConditinalRendering/Enable_Disable_Button.jsx
+++ src/Components/ConditinalRendering/Enable_Disable_Button.jsx
@@ -9,7 +9,7 @@ export default class Enable_Disable_Button extends Component {
         return (
             <div>
                 <form>
-                    <input className="in_field" type="text" placeholder="User Id"/>
+                    <input className="in_field" type="text" placeholder="User Id" onChange={this.handleChange}/>
                     <button className="btn_proceed" disabled={this.state.disabled}>Proceed</button>
                 </form>
                 
