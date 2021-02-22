d08ceebcc6fc746ba269c6f2dc60abd8235a64e5
mohammadriz64 <76685820+mohammadriz64@users.noreply.github.com>
2021-02-22 21:20:08 +0530

    btn styling

--- src/Components/ConditinalRendering/Enable_Disable_Button.jsx
+++ src/Components/ConditinalRendering/Enable_Disable_Button.jsx
@@ -7,7 +7,7 @@ export default class Enable_Disable_Button extends Component {
             <div>
                 <form>
                     <input className="in_field" type="text" placeholder="User Id"/>
-                    <button disabled="true">Proceed</button>
+                    <button className="btn_proceed" disabled="true">Proceed</button>
                 </form>
                 
             </div>
