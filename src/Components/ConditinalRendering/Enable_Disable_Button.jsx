e2d99786ce1d31ccaaf6107ce6b4d16e3d7253b5
mohammadriz64 <76685820+mohammadriz64@users.noreply.github.com>
2021-02-22 21:33:21 +0530

    onchangeevent

--- src/Components/ConditinalRendering/Enable_Disable_Button.jsx
+++ src/Components/ConditinalRendering/Enable_Disable_Button.jsx
@@ -1,19 +1,33 @@
-import React, { Component } from 'react'
-import './Enable_Disable.css';
+import React, { Component } from "react";
+import "./Enable_Disable.css";
 
 export default class Enable_Disable_Button extends Component {
-    state = {
-        disabled: true
-    }
-    render() {
-        return (
-            <div>
-                <form>
-                    <input className="in_field" type="text" placeholder="User Id" onChange={this.handleChange}/>
-                    <button className="btn_proceed" disabled={this.state.disabled}>Proceed</button>
-                </form>
-                
-            </div>
-        )
-    }
+  state = {
+    disabled: true,
+  };
+
+  handleChange =(e) =>{
+      if(e.target.value.length >=6) {
+          this.setState({
+              disabled: false
+          })
+      }
+  }
+  render() {
+    return (
+      <div>
+        <form>
+          <input
+            className="in_field"
+            type="text"
+            placeholder="User Id"
+            onChange={this.handleChange}
+          />
+          <button className="btn_proceed" disabled={this.state.disabled}>
+            Proceed
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
