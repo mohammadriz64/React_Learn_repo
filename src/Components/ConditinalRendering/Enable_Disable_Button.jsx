59dd011c72c88fb5dd42f9d7a47f2b61e81e9006
mohammadriz64 <76685820+mohammadriz64@users.noreply.github.com>
2021-02-22 21:23:47 +0530

    creating state

--- src/Components/ConditinalRendering/Enable_Disable_Button.jsx
+++ src/Components/ConditinalRendering/Enable_Disable_Button.jsx
@@ -2,7 +2,9 @@ import React, { Component } from 'react'
 import './Enable_Disable.css';
 
 export default class Enable_Disable_Button extends Component {
-    
+    state = {
+        disabled: true
+    }
     render() {
         return (
             <div>
