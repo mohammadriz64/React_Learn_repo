51470846778bb2ef9c3d2a7da1d8a36aa76a08fe
mohammadriz64 <76685820+mohammadriz64@users.noreply.github.com>
2021-02-23 15:09:32 +0530

    using use selector in parent user

--- src/Components/Practice/FetchingFromRedux/UserDataFetchingPar.jsx
+++ src/Components/Practice/FetchingFromRedux/UserDataFetchingPar.jsx
@@ -1,8 +1,14 @@
 import React,{useState,useEffect} from 'react';
+import UserDataFetchingChild from './UserDataFetchingChild';
+import {useSelector} from 'react-redux';
+import {getAllUsers} from '../../../State/Selector/usersInGroupSelector';
 
 const UserDataFetchingPar = () => {
+    const selector=useSelector(getAllUsers);
+    
     return (
         <div>
+            <UserDataFetchingChild/>
             
         </div>
     );
