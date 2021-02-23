948af6ade51e7295a8adda805554784a928a4c0d
mohammadriz64 <76685820+mohammadriz64@users.noreply.github.com>
2021-02-23 15:03:14 +0530

    importing selector in parent file

--- src/Components/Practice/FetchingFromRedux/FetchingDataReduxPar.jsx
+++ src/Components/Practice/FetchingFromRedux/FetchingDataReduxPar.jsx
@@ -2,6 +2,7 @@ import React,{useEffect,useState} from 'react';
 import FetchingDataReduxChild from './FetchingDataReduxChild';
 import {useSelector} from 'react-redux';
 import {getAllGroups} from '../../../State/Selector/groupSelector';
+import {getAllUsers} from '../../../State/Selector/usersInGroupSelector';
 
 
 const FetchingDataReduxPar = () => {
