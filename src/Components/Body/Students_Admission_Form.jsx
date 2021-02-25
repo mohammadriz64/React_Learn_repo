f212a2c1f858d15a99c59a624c619e4304914768
mohammadriz64 <76685820+mohammadriz64@users.noreply.github.com>
2021-02-25 19:17:17 +0530

    styling 1st input

--- src/Components/Body/Students_Admission_Form.jsx
+++ src/Components/Body/Students_Admission_Form.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import './Student_Form.css';
 
 export const Students_Admission_Form = () => {
     return (
         <div>
-                <h3>Academic Details:(Inter/12th Std. Details)</h3>
+                <h3 className="Acad_Heading">Academic Details:(Inter/12th Std. Details)</h3>
             <form>
-                <label>Name of Exam</label><input type="text"/>
+                <label>Name of Exam</label><input className="student_input" type="text"/>
 
             </form>
         </div>
