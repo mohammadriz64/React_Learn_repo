018ca50cc975393635fc319ca48a7a93b9a211b4
mohammadriz64 <76685820+mohammadriz64@users.noreply.github.com>
2021-02-25 19:26:12 +0530

    other inputs

--- src/Components/Body/Students_Admission_Form.jsx
+++ src/Components/Body/Students_Admission_Form.jsx
@@ -8,7 +8,8 @@ export const Students_Admission_Form = () => {
             <form>
                 <label>Name of Exam :</label><input className="student_input" type="text"/> <br/>
                 <label>Board of Exam :</label><input className="student_input" type="text"/> <br/>
-                <label>Name of School/college :</label><input className="student_input" type="text"/>
+                <label>Name of School/college :</label><input className="student_input" type="text"/> <br/>
+                <label>HallTicket No :</label><input className="input_inline" type="text"/><label>Total Marks Obtained :</label><input type="text"/><label>Maximum Marks :</label><input type="text"/>
 
             </form>
         </div>
