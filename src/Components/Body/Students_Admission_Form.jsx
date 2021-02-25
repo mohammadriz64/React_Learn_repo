7b82305db9fce744f9a9b1bb910c2a82a7008dc2
mohammadriz64 <76685820+mohammadriz64@users.noreply.github.com>
2021-02-25 19:22:24 +0530

    styling to 2nd and 3rd inputs

--- src/Components/Body/Students_Admission_Form.jsx
+++ src/Components/Body/Students_Admission_Form.jsx
@@ -6,7 +6,9 @@ export const Students_Admission_Form = () => {
         <div>
                 <h3 className="Acad_Heading">Academic Details:(Inter/12th Std. Details)</h3>
             <form>
-                <label>Name of Exam</label><input className="student_input" type="text"/>
+                <label>Name of Exam :</label><input className="student_input" type="text"/> <br/>
+                <label>Board of Exam :</label><input className="student_input" type="text"/> <br/>
+                <label>Name of School/college :</label><input className="student_input" type="text"/>
 
             </form>
         </div>
