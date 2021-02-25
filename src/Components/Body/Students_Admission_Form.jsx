ea34f7eae842991c25f13978e6ffb7eac99547ce
mohammadriz64 <76685820+mohammadriz64@users.noreply.github.com>
2021-02-25 19:34:38 +0530

    upper page part done

--- src/Components/Body/Students_Admission_Form.jsx
+++ src/Components/Body/Students_Admission_Form.jsx
@@ -1,17 +1,34 @@
-import React from 'react';
-import './Student_Form.css';
+import React from "react";
+import "./Student_Form.css";
 
 export const Students_Admission_Form = () => {
-    return (
-        <div>
-                <h3 className="Acad_Heading">Academic Details:(Inter/12th Std. Details)</h3>
-            <form>
-                <label>Name of Exam :</label><input className="student_input" type="text"/> <br/>
-                <label>Board of Exam :</label><input className="student_input" type="text"/> <br/>
-                <label>Name of School/college :</label><input className="student_input" type="text"/> <br/>
-                <label>HallTicket No :</label><input className="input_inline" type="text"/><label>Total Marks Obtained :</label><input className="input_inline" type="text"/><label>Maximum Marks :</label><input className="input_inline" type="text"/>
-
-            </form>
-        </div>
-    );
+  return (
+    <div>
+      <h3 className="Acad_Heading">
+        Academic Details:(Inter/12th Std. Details)
+      </h3>
+      <form>
+        <label>Name of Exam :</label>
+        <input className="student_input" type="text" /> <br />
+        <label>Board of Exam :</label>
+        <input className="student_input" type="text" /> <br />
+        <label>Name of School/college :</label>
+        <input className="student_input" type="text" /> <br />
+        <label>HallTicket No :</label>
+        <input className="input_inline" type="text" />
+        <label>Total Marks Obtained :</label>
+        <input className="input_inline" type="text" />
+        <label>Maximum Marks :</label>
+        <input className="input_inline" type="text" /> <br/>
+        <label>Passing Month :</label>
+        <input className="input_inline" type="text" />
+        <label>Year :</label>
+        <input className="input_inline" type="text" />
+        <label>Percentage Marks Scored :</label>
+        <input className="input_inline" type="text" />
+        <label>Grade :</label>
+        <input className="input_inline" type="text" />
+      </form>
+    </div>
+  );
 };
