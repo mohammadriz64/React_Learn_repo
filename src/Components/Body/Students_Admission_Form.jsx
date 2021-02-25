d03b647e3fa132c79d3b85f48a704527754b784a
mohammadriz64 <76685820+mohammadriz64@users.noreply.github.com>
2021-02-25 19:09:44 +0530

    starting form

--- src/Components/Body/Students_Admission_Form.jsx
+++ src/Components/Body/Students_Admission_Form.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 export const Students_Admission_Form = () => {
     return (
         <div>
-            
+            <form>
+                <h3>Academic Details:(Inter/12th Std. Details)</h3>
+            </form>
         </div>
     );
 };
