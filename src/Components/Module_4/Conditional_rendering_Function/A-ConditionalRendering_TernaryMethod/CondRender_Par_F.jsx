370fd5af76eaaf35871488f6e1296cbfc3620914
mohammadriz64 <76685820+mohammadriz64@users.noreply.github.com>
2021-03-02 19:58:11 +0530

    making data

--- src/Components/Module_4/Conditional_rendering_Function/A-ConditionalRendering_TernaryMethod/CondRender_Par_F.jsx
+++ src/Components/Module_4/Conditional_rendering_Function/A-ConditionalRendering_TernaryMethod/CondRender_Par_F.jsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CondRender_Child_F from './CondRender_Child_F';
 
 const CondRender_Par_F = () => {
+    const [personDetails, setPersonDetails] = usestate({
+        details: [
+
+            { name: "React Js", course: "Js Library for Front-End" },
+            { name: "Node Js", course: "Js Library for Backend " },
+            { name: "React", course: "Js Library" },
+
+        ],
+    })
     return (
         <div>
             <CondRender_Child_F />
