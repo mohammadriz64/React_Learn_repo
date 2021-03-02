83067f193cfebb1609e57b7c459965fbb48f085f
mohammadriz64 <76685820+mohammadriz64@users.noreply.github.com>
2021-03-02 19:46:19 +0530

    cond rendering parent file

---
+++ src/Components/Module_4/Conditional_rendering_Function/A-ConditionalRendering_TernaryMethod/CondRender_Par_F.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CondRender_Par_F = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default CondRender_Par_F;
--- src/Components/Module_4/ModuleFour_Landing.jsx
+++ src/Components/Module_4/ModuleFour_Landing.jsx
@@ -1,8 +1,15 @@
 import React, { memo } from 'react';
+import { Row, Col } from 'react-bootstrap';
 
 const ModuleFour_Landing = () => {
     return (
         <div>
+            <h2>Conditional Rending</h2>
+            <Row>
+                <Col xs={6} lg={6} md={6}>
+
+                </Col>
+            </Row>
 
         </div>
     );
