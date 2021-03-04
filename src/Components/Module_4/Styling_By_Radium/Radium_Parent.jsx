8e1a7fd0199b096dad17281baadd6459924a304d
mohammadriz64 <76685820+mohammadriz64@users.noreply.github.com>
2021-03-04 16:36:09 +0530

    giving color js part

--- src/Components/Module_4/Styling_By_Radium/Radium_Parent.jsx
+++ src/Components/Module_4/Styling_By_Radium/Radium_Parent.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
 const Radium_Parent = () => {
+    const styleButton = {
+        backgroundColor: "green",
+        boxShadow: "2px 2px 2px 2px black",
+    };
     return (
         <div>
             <h1>Radium Module Practice</h1>
